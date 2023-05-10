83f49ee6b55b844cf53a4672d0607b7bff4ea0a9
adelbert-samuel <s.k.adelbert@gmail.com>
2023-05-10 00:22:02 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,76 +3,57 @@
  * File: 0-binary_to_uint.c
  */
 
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-/**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *         Otherwise - the converted number.
- */
-unsigned int binary_to_uint(const char *b)
-{
+unsigned int binary_to_uint(const char *b) {
+    // Check if the input string is NULL
+    if (b == NULL)
+        return 0;
 
-	int len = 0, i;
-	unsigned int sum = 0;
+    // Get the length of the input string
+    size_t length = strlen(b);
 
-	if (b == NULL)
-		return (sum);
+    // Variable to store the converted number
+    unsigned int result = 0;
 
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
+    // Iterate through each character in the string
+    for (size_t i = 0; i < length; i++) {
+        // Check if the character is '0' or '1'
+        if (b[i] == '0') {
+            // Left-shift the result and add 0
+            result = (result << 1) + 0;
+        } else if (b[i] == '1') {
+            // Left-shift the result and add 1
+            result = (result << 1) + 1;
+        } else {
+            // Invalid character encountered, return 0
+            return 0;
+        }
+    }
 
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
-	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
-	}
-
-	return (sum);
+    // Return the converted number
+    return result;
 }
 
-/*
- * alternative method not using bitwise but a power of 2
- *
- *
- * unsigned int binary_to_uint(const char *b)
- *{
- *
- *	int len = 0, pow = 1;
- *	unsigned int sum = 0, error = 0;
- *
- *	if (b == NULL)
- *		return (error);
- *
- *	while (b[len] != '\0') // find string length
- *		len++;
- *	len -= 1;
- *
- *	while (len >= 0) // iterate from back of string
- *	{
- *		if ((b[len] != '0') && (b[len] != '1'))
- *			return (error);
- *
- *		if (b[len] == '1') // add appropriate power of 2 if '1'
- *			sum += pow;
- *
- *		pow *= 2; // update power of 2
- *		len--;
- *	}
- *
- *	return (sum);
- *}
- */
+int main() {
+    const char *binary1 = "101010";
+    unsigned int decimal1 = binary_to_uint(binary1);
+    printf("Binary: %s\nUnsigned Int: %u\n\n", binary1, decimal1);
+
+    const char *binary2 = "110011";
+    unsigned int decimal2 = binary_to_uint(binary2);
+    printf("Binary: %s\nUnsigned Int: %u\n\n", binary2, decimal2);
+
+    const char *binary3 = "1234";  // Invalid character '3'
+    unsigned int decimal3 = binary_to_uint(binary3);
+    printf("Binary: %s\nUnsigned Int: %u\n\n", binary3, decimal3);
+
+    const char *binary4 = NULL;  // NULL string
+    unsigned int decimal4 = binary_to_uint(binary4);
+    printf("Binary: %s\nUnsigned Int: %u\n\n", binary4, decimal4);
+
+    return 0;
+}
 
