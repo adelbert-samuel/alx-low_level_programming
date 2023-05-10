e0174a660cd6cc78acc68d81f943ffb832f04d9a
adelbert-samuel <s.k.adelbert@gmail.com>
2023-05-10 00:29:59 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,57 +3,34 @@
  * File: 0-binary_to_uint.c
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
-unsigned int binary_to_uint(const char *b) {
-    // Check if the input string is NULL
-    if (b == NULL)
-        return 0;
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: A pointer to a string of 0 and 1 chars.
+ *
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mult = 1;
+	int len;
 
-    // Get the length of the input string
-    size_t length = strlen(b);
+	if (b == '\0')
+		return (0);
 
-    // Variable to store the converted number
-    unsigned int result = 0;
+	for (len = 0; b[len];)
+		len++;
 
-    // Iterate through each character in the string
-    for (size_t i = 0; i < length; i++) {
-        // Check if the character is '0' or '1'
-        if (b[i] == '0') {
-            // Left-shift the result and add 0
-            result = (result << 1) + 0;
-        } else if (b[i] == '1') {
-            // Left-shift the result and add 1
-            result = (result << 1) + 1;
-        } else {
-            // Invalid character encountered, return 0
-            return 0;
-        }
-    }
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
 
-    // Return the converted number
-    return result;
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+
+	return (num);
 }
-
-int main() {
-    const char *binary1 = "101010";
-    unsigned int decimal1 = binary_to_uint(binary1);
-    printf("Binary: %s\nUnsigned Int: %u\n\n", binary1, decimal1);
-
-    const char *binary2 = "110011";
-    unsigned int decimal2 = binary_to_uint(binary2);
-    printf("Binary: %s\nUnsigned Int: %u\n\n", binary2, decimal2);
-
-    const char *binary3 = "1234";  // Invalid character '3'
-    unsigned int decimal3 = binary_to_uint(binary3);
-    printf("Binary: %s\nUnsigned Int: %u\n\n", binary3, decimal3);
-
-    const char *binary4 = NULL;  // NULL string
-    unsigned int decimal4 = binary_to_uint(binary4);
-    printf("Binary: %s\nUnsigned Int: %u\n\n", binary4, decimal4);
-
-    return 0;
-}
-
